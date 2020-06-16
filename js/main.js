67096d1d99011c69798d3f9a33e99634cda9b0c8
Maksim Maltcev <maximus3506@mail.ru>
2020-06-16 21:20:53 +0300

    added cnsole-log and comments

--- js/main.js
+++ js/main.js
@@ -1,22 +1,24 @@
 'use strict';
 
-const   start = document.getElementById('start'),
-        incomeAdd = document.getElementsByTagName('button')[0],
-        expensesAdd = document.getElementsByTagName('button')[1],
-        depositCheck = document.querySelector('#deposit-check'),
-        additionalIncomeItem = document.querySelectorAll('.additional_income-item'),               
-        budgetMonth = document.getElementsByClassName('budget_month-value')[0],
-        budgetDay = document.getElementsByClassName('budget_day-value')[0],
-        expensesMonth = document.getElementsByClassName('expenses_month-value')[0],
-        additionalIncome = document.getElementsByClassName('additional_income-value')[0],
-        additionalExpenses = document.getElementsByClassName('additional_expenses-value')[0],
-        incomePeriod = document.getElementsByClassName('income_period-value')[0],
-        targetMonth = document.getElementsByClassName('target_month-value')[0],
-        salaryAmount = document.querySelector('.salary-amount'),
-        incomeTitle = document.querySelector('.income-title'),
-        incomeAmount = document.querySelector('.income-amount'),
-        expensesTitle = document.querySelector('.expenses-title'),
-        expensesAmount = document.querySelector('.expenses-amount'),
-        additionalEpensesItem = document.querySelector('.additional_expenses-item'),
-        periodSelect = document.querySelector('.period-select');
+const   start = document.getElementById('start'), //кнопка расчитать
+        incomeAdd = document.getElementsByTagName('button')[0], //добавить поля дохода
+        expensesAdd = document.getElementsByTagName('button')[1], //добавить поля расходов
+        depositCheck = document.querySelector('#deposit-check'), // чекбокс депозита
+        additionalIncomeItem = document.querySelectorAll('.additional_income-item'), //поля возвожных доходов              
+        budgetMonth = document.getElementsByClassName('budget_month-value')[0], //поле дохода за месяц
+        budgetDay = document.getElementsByClassName('budget_day-value')[0], //поле дневного бюджета
+        expensesMonth = document.getElementsByClassName('expenses_month-value')[0],//поле расходов за месяц
+        additionalIncome = document.getElementsByClassName('additional_income-value')[0], // возможные доходы
+        additionalExpenses = document.getElementsByClassName('additional_expenses-value')[0], //возможные расходы
+        incomePeriod = document.getElementsByClassName('income_period-value')[0], //накопления за период
+        targetMonth = document.getElementsByClassName('target_month-value')[0], // срок достижения цели
+        salaryAmount = document.querySelector('.salary-amount'), //месячный доход
+        incomeTitle = document.querySelector('.income-title'), //наименование доп дохода
+        incomeAmount = document.querySelector('.income-amount'), //сумма доп дохода
+        expensesTitle = document.querySelector('.expenses-title'), //наименование обязат расходов
+        expensesAmount = document.querySelector('.expenses-amount'), //сумма обязательных расходов
+        additionalEpensesItem = document.querySelector('.additional_expenses-item'), // возможные расходы
+        periodSelect = document.querySelector('.period-select'); // тумблер
+
+console.log(start, incomeAdd, expensesAdd, depositCheck, additionalIncomeItem, budgetDay, expensesMonth, additionalIncome, additionalExpenses, incomePeriod, targetMonth, salaryAmount, incomeTitle, incomeAmount, expensesTitle, expensesAmount, additionalEpensesItem, periodSelect );
 
